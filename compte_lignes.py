e9af973a096663f6c01616dc26dacdd6bc15c49a
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-10-15 16:54:55 +0200

    -> ch10 fonctions

---
+++ compte_lignes.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("ERREUR : il faut exactement un argument.")
+
+nom_fichier = sys.argv[1]
+taille = 0
+with open(nom_fichier, "r") as f_in:
+    taille = len(f_in.readlines())
+
+print(f"{nom_fichier} contient {taille} lignes.")
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print('Hello world!')
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit("ERREUR : il faut exactement un argument.")
+
+print(f"Argument vaut : {sys.argv[1]}")
